cb96a9e2bbc8cc391f67c1f31d9fb0aee585aa88
ombhojane <ombhojane05@gmail.com>
2023-12-15 20:03:21 +0530

    fix v2

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -15,10 +15,12 @@ export default function HomePage() {
     <div>
       <div className="mb-8 space-y-4">
         <h2 className="text-2xl md:text-4xl font-bold text-center">
-          Explore the power of AI
+          Welcome to Truecaptions
         </h2>
         <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
-          Chat with the smartest AI - Experience the power of AI
+          Explore dynamic features to validate your content
+        </p>
+        <p className="text-muted-foreground font-light text-sm md:text-lg text-center">More info will be added here
         </p>
       </div>
       <div className="px-4 md:px-20 lg:px-32 space-y-4">
--- constants.ts
+++ constants.ts
@@ -1,4 +1,4 @@
-import { TextIcon } from "lucide-react";
+import { AlignHorizontalDistributeCenter, Image, Newspaper, TextIcon } from "lucide-react";
 import { Code, ImageIcon, MessageSquare, Music, VideoIcon } from "lucide-react";
 
 export const MAX_FREE_COUNTS = 5;
@@ -11,32 +11,26 @@ export const tools = [
     color: "text-violet-500",
     bgColor: "bg-violet-500/10",
   },
+  
   {
-    label: 'Music Generation',
-    icon: Music,
-    href: '/music',
-    color: "text-emerald-500",
-    bgColor: "bg-emerald-500/10",
-  },
-  {
-    label: 'Image Generation',
+    label: 'Image Insights',
     icon: ImageIcon,
-    color: "text-pink-700",
-    bgColor: "bg-pink-700/10",
+    color: "text-violet-700",
+    bgColor: "bg-violet-700/10",
     href: '/image',
   },
   {
-    label: 'Video Generation',
-    icon: VideoIcon,
+    label: 'Fake News Detection',
+    icon: Newspaper,
     color: "text-orange-700",
     bgColor: "bg-orange-700/10",
     href: '/video',
   },
   {
-    label: 'Code Generation',
-    icon: Code,
+    label: 'Documentation',
+    icon: AlignHorizontalDistributeCenter,
     color: "text-green-700",
     bgColor: "bg-green-700/10",
-    href: '/code',
+    href: '/settings',
   },
 ];
